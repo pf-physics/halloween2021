20db7df52d8a03a11a6a7761c02423b3f26b50cd
Sacha Perry-Fagant <perryfagant.sacha@gmail.com>
2021-10-18 11:25:58 +0200

    Updates

---
+++ precache-manifest.e51a6bb4f7a091da0bda52d2a67f7a30.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac03a222cca1426e1814b4d92bb66ce",
+    "url": "/halloween2021/index.html"
+  },
+  {
+    "revision": "73925978bfe4a6e0658c",
+    "url": "/halloween2021/static/css/2.29fb477d.chunk.css"
+  },
+  {
+    "revision": "275b950b41912e4c0f3f",
+    "url": "/halloween2021/static/css/main.04a73c5b.chunk.css"
+  },
+  {
+    "revision": "73925978bfe4a6e0658c",
+    "url": "/halloween2021/static/js/2.6de8f933.chunk.js"
+  },
+  {
+    "revision": "2aff8cf35e0d5ba1e4add0f1b477519f",
+    "url": "/halloween2021/static/js/2.6de8f933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275b950b41912e4c0f3f",
+    "url": "/halloween2021/static/js/main.ae3b5a71.chunk.js"
+  },
+  {
+    "revision": "36816eabab18feff0c28",
+    "url": "/halloween2021/static/js/runtime-main.6dd9221b.js"
+  },
+  {
+    "revision": "ce35e3f937bb4c47cf1b19a10c6037a9",
+    "url": "/halloween2021/static/media/Clownoween.ce35e3f9.ttf"
+  },
+  {
+    "revision": "67832539cd5b28e947b539f456bdfa14",
+    "url": "/halloween2021/static/media/Ending.67832539.mp3"
+  },
+  {
+    "revision": "8910d09c871fcdd943df3a68bf676a56",
+    "url": "/halloween2021/static/media/PhantomFingers.8910d09c.ttf"
+  },
+  {
+    "revision": "b9abce387bdd6a6a4a8a9f4de75a237a",
+    "url": "/halloween2021/static/media/ScarySound.b9abce38.mp3"
+  },
+  {
+    "revision": "edeca8362649a2e04b5ab486c5a4ec4b",
+    "url": "/halloween2021/static/media/beforeEnding.edeca836.mp3"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/halloween2021/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/halloween2021/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/halloween2021/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/halloween2021/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/halloween2021/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/halloween2021/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0c046ed5c6e59d586e27763446156211",
+    "url": "/halloween2021/static/media/ghost.0c046ed5.png"
+  },
+  {
+    "revision": "19685a1635f8e06dcb118c76f32b4267",
+    "url": "/halloween2021/static/media/ghost.19685a16.mp3"
+  },
+  {
+    "revision": "873f9c09f18ed497bdccda274d6e3945",
+    "url": "/halloween2021/static/media/host.873f9c09.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/halloween2021/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/halloween2021/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/halloween2021/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/halloween2021/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/halloween2021/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "606c53a56c7cd816095a929067de4a6b",
+    "url": "/halloween2021/static/media/intro.606c53a5.mp3"
+  },
+  {
+    "revision": "1060be38db8ba39d43bcfbf17f8ee902",
+    "url": "/halloween2021/static/media/moon.1060be38.jpg"
+  },
+  {
+    "revision": "c5d8fb3ffa142ebb5c3eb91ceec663b7",
+    "url": "/halloween2021/static/media/ominous.c5d8fb3f.mp3"
+  },
+  {
+    "revision": "9ef5d9c0e92a2e5444f7afa752867669",
+    "url": "/halloween2021/static/media/oracle1.9ef5d9c0.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/halloween2021/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/halloween2021/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/halloween2021/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/halloween2021/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/halloween2021/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "45e86177ba499749bd0f524068e1c1cb",
+    "url": "/halloween2021/static/media/rats.45e86177.mp3"
+  },
+  {
+    "revision": "7b5b05585f22895e73792cee240bf6bd",
+    "url": "/halloween2021/static/media/scarycandy.7b5b0558.otf"
+  },
+  {
+    "revision": "14868e5b72b025a2c2c9da3d2966e9d7",
+    "url": "/halloween2021/static/media/skeleton.14868e5b.jpg"
+  },
+  {
+    "revision": "3c82e6ef2b5cdc40bc71f4be27f15414",
+    "url": "/halloween2021/static/media/skeleton.3c82e6ef.mp3"
+  },
+  {
+    "revision": "2733ae0a1c8340642478f8288c5b1737",
+    "url": "/halloween2021/static/media/spider6.2733ae0a.jpg"
+  },
+  {
+    "revision": "2af1ad97cb9f5b48b87f4a2dfdf00aec",
+    "url": "/halloween2021/static/media/spiderRune.2af1ad97.mp3"
+  },
+  {
+    "revision": "139947b8dd0ae76b16c2f4cc14bf3a2d",
+    "url": "/halloween2021/static/media/vampire.139947b8.jpg"
+  },
+  {
+    "revision": "be88452be9d17a2a785f34acdc63698c",
+    "url": "/halloween2021/static/media/vampire.be88452b.mp3"
+  },
+  {
+    "revision": "b64bb8f925152508fe2c422c0d93e8b9",
+    "url": "/halloween2021/static/media/werewolf.b64bb8f9.mp3"
+  },
+  {
+    "revision": "1b94668550dacb0a28153da55aefe606",
+    "url": "/halloween2021/static/media/witch.1b946685.mp3"
+  },
+  {
+    "revision": "c8c03594ccf3a3be5cb47b1cc0061a26",
+    "url": "/halloween2021/static/media/wolf1.c8c03594.png"
+  },
+  {
+    "revision": "3c630cd90a54bfb788f77b8f9e1b5212",
+    "url": "/halloween2021/static/media/wolf2.3c630cd9.png"
+  },
+  {
+    "revision": "dbdd6919ff42e2dae896c767bc56898f",
+    "url": "/halloween2021/static/media/wolf2.dbdd6919.jpg"
+  },
+  {
+    "revision": "92bcd063a8478dc4f71eebe04c891186",
+    "url": "/halloween2021/static/media/wolf3.92bcd063.png"
+  },
+  {
+    "revision": "04bc4d705769d1b9b2eae335871bbb7d",
+    "url": "/halloween2021/static/media/youmurdererbb_reg.04bc4d70.ttf"
+  },
+  {
+    "revision": "d43b63349e66d1713f92200c2a8ec2d6",
+    "url": "/halloween2021/static/media/zombie.d43b6334.jpg"
+  },
+  {
+    "revision": "e473f5eaf55974d7308182d9f00ead3e",
+    "url": "/halloween2021/static/media/zombie.e473f5ea.mp3"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/halloween2021/precache-manifest.e51a6bb4f7a091da0bda52d2a67f7a30.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/halloween2021/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
